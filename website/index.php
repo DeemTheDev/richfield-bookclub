053b042f6edb92a396647696bb7d89f149942ce0
DeemTheDev <136303631+DeemTheDev@users.noreply.github.com>
2023-10-24 00:31:30 +0200

    craeted classes and sections for the index page.

---
+++ website/index.php
@@ -0,0 +1,45 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Please include this CSS file in all pages.-->
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Home</title>
+</head>
+<body>
+   <header class="header container">
+        <nav>
+            <ul class="header__logo">
+                <li><img class="header__img" src="web-media/logo (2).png" alt="logo"></li>
+                <li><a class="header__logolink" href="index.php">BOOK CLUB</a></li>
+            </ul>
+            <ul class="header__menu">
+                <li><a class="header__links" href="community.php">COMMUNITY</a></li>
+                <li><a class="header__links" href="admin.php">ADMIN</a></li>
+            </ul>
+        </nav>
+   </header>
+   <main class="container">
+    <section class="current">
+        <div class="curren__book"></div>
+        <div class="current__summary"></div>
+        <div class="current__time"></div>
+        <div class="current__download"></div>
+    </section>
+    <section class="books">
+
+        <div class="books__button">
+            <a href="#" class="button">
+                <div class="button__line"></div>
+                <div class="button__line"></div>
+                <span class="button__text">UPLOAD</span>
+                <div class="button__drow1"></div>
+                <div class="button__drow2"></div>
+            </a>
+        </div>
+    </section>
+   </main>
+   <footer></footer>
+</body>
+</html>
