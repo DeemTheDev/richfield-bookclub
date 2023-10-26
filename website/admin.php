e038d1ddcff34ed8de72991d4c4b4ec13a8e1937
Vivi0420 <vivashsingh001@gmail.com>
2023-10-26 21:02:19 +0200

    Admin page

---
+++ website/admin.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Book Club</title>
+  <style>
+    
+    table {
+      border-collapse: collapse;
+      width: 100%;
+    }
+    th, td {
+      border: 1px solid black;
+      padding: 8px;
+      text-align: left;
+    }
+  </style>
+</head>
+<body>
+  <h1>Book Club</h1>
+
+  <h2>Add/Remove Students</h2>
+  <form>
+    
+    <label for="addStudent">Add Student:</label>
+    <input type="text" id="addStudent" />
+    <button type="button" onclick="addStudent()">Add</button>
+
+    
+    <label for="removeStudent">Remove Student:</label>
+    <input type="text" id="removeStudent" />
+    <button type="button" onclick="removeStudent()">Remove</button>
+  </form>
+
+  <h2>Most Voted Books</h2>
+  <table id="mostVotedTable">
+  
+    <tr>
+      <th>Rank</th>
+      <th>Title</th>
+      <th>Votes</th>
+      <th>Start Date</th>
+      <th>Finish Date</th>
+    </tr>
+  </table>
+
+  <h2>Vote for Books</h2>
+  <form>
+   
+    <label for="bookDropdown">Choose a book:</label>
+    <select id="bookDropdown">
+      <option value="book1">Book 1</option>
+      <option value="book2">Book 2</option>
+      <option value="book3">Book 3</option>
+      <option value="book4">Book 4</option>
+    </select>
+    <button type="button" onclick="voteForBook()">Choose</button>
+  </form>
+
+  <script>
+    // Initialize the students array and bookVotes object
+    var students = [];
+    var bookVotes = {
+      "book1": { votes: 0, startDate: "", finishDate: "" },
+      "book2": { votes: 0, startDate: "", finishDate: "" },
+      "book3": { votes: 0, startDate: "", finishDate: "" },
+      "book4": { votes: 0, startDate: "", finishDate: "" }
+    };
+
+   
+    function addStudent() {
+      var studentInput = document.getElementById("addStudent");
+      var studentName = studentInput.value;
+      if (studentName) {
+        students.push(studentName);
+        studentInput.value = "";
+        updateStudentsTable();
+      }
+    }
+
+
+    function removeStudent() {
+      var studentInput = document.getElementById("removeStudent");
+      var studentName = studentInput.value;
+      var index = students.indexOf(studentName);
+      if (index > -1) {
+        students.splice(index, 1);
+        studentInput.value = "";
+        updateStudentsTable();
+      }
+    }
+
+    // Function to update the most voted books table
+    function updateStudentsTable() {
+      var table = document.getElementById("mostVotedTable");
+      table.innerHTML = `
+        <tr>
+          <th>Rank</th>
+          <th>Title</th>
+          <th>Votes</th>
+          <th>Start Date</th>
+          <th>Finish Date</th>
+        </tr>
+      `;
+      // Sort the books based on votes in descending order
+      var sortedVotes = Object.keys(bookVotes).sort(function(a, b) {
+        return bookVotes[b].votes - bookVotes[a].votes;
+      });
+      // Display the top 3 voted books in the table
+      for (var i = 0; i < 3; i++) {
+        var book = sortedVotes[i];
+        var row = table.insertRow(-1);
+        var rankCell = row.insertCell(0);
+        var titleCell = row.insertCell(1);
+        var votesCell = row.insertCell(2);
+        var startDateCell = row.insertCell(3);
+        var finishDateCell = row.insertCell(4);
+        rankCell.innerHTML = i + 1; // Display the rank
+        titleCell.innerHTML = book; // Display the book title
+        votesCell.innerHTML = bookVotes[book].votes; // Display the vote count
+        startDateCell.innerHTML = `<input type="date" id="${book}-start-date" onchange="updateStartDate('${book}')">`; // Display the start date input
+        finishDateCell.innerHTML = `<input type="date" id="${book}-finish-date" onchange="updateFinishDate('${book}')">`; // Display the finish date input
+      }
+    }
+
+    // Function to vote for a book
+    function voteForBook() {
+      var bookDropdown = document.getElementById("bookDropdown");
+      var selectedBook = bookDropdown.value;
+      if (selectedBook) {
+        bookVotes[selectedBook].votes++;
+        updateStudentsTable();
+      }
+    }
+
+    // Function to update the start date for a book
+    function updateStartDate(book) {
+      var startDateInput = document.getElementById(`${book}-start-date`);
+      bookVotes[book].startDate = startDateInput.value;
+    }
+
+    // Function to update the finish date for a book
+    function updateFinishDate(book) {
+      var finishDateInput = document.getElementById(`${book}-finish-date`);
+      bookVotes[book].finishDate = finishDateInput.value;
+    }
+  </script>
+</body>
+</html>
