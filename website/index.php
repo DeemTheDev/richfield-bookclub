daba2ad847f96ac1dc0e6167342ef2691294fa5d
DeemTheDev <136303631+DeemTheDev@users.noreply.github.com>
2023-10-27 01:16:27 +0200

    responsivness and grid

--- website/index.php
+++ website/index.php
@@ -13,11 +13,15 @@
         <nav>
             <ul class="header__logo">
                 <li><img class="header__img" src="web-media/logo (2).png" alt="logo"></li>
-                <li><a class="header__logolink" href="index.php">BOOK CLUB</a></li>
             </ul>
             <ul class="header__menu">
+                <div class="header__dropdown">
                 <li><a class="header__links" href="community.php">COMMUNITY</a></li>
+                <li><a class="header__links" href="login.php">LOG OUT</a></li>
                 <li><a class="header__links" href="admin.php">ADMIN</a></li>
+                </div>
+            
+            <label for="nav" style="color: white;" id="toggle">&#9776;</label>
             </ul>
         </nav>
    </header>
@@ -31,14 +35,36 @@
     <section class="books">
         <?php
             for($i = 1; $i <= 3; $i++){
-                echo "<div class='books__pdf'>";
-            for($z=1; $z <=4; $z++){
-                echo "<div class='books__img'>
-                <embed src='books/Atomic-Habits-old.pdf' height='90%' width='100%'>
-                <div class='books__download'><a href='books/Atomic-Habits-old.pdf'><button>DOWNLOAD</button></a></div>
-                </div>";
-            }
-                 echo "</div>";
+                echo "<div class='books__pdf'>
+                        <div class='books__img item1'>
+                            <embed src='books/Atomic-Habits-old.pdf' height='90%' width='100%'>
+                            <div class='books__buttons'>
+                                <div class='books__download'><a href='books/Atomic-Habits-old.pdf'><button>DOWNLOAD</button></a></div>
+                                <div class='books__vote'><span>VOTE: <input type='checkbox' name='vote'></span></div>
+                            </div>
+                        </div>
+                        <div class='books__img item2'>
+                            <embed src='books/Atomic-Habits-old.pdf' height='90%' width='100%'>
+                            <div class='books__buttons'>
+                                <div class='books__download'><a href='books/Atomic-Habits-old.pdf'><button>DOWNLOAD</button></a></div>
+                                <div class='books__vote'><span>VOTE: <input type='checkbox' name='vote'></span></div>
+                            </div>
+                        </div>
+                        <div class='books__img item3'>
+                            <embed src='books/Atomic-Habits-old.pdf' height='90%' width='100%'>
+                            <div class='books__buttons'>
+                                <div class='books__download'><a href='books/Atomic-Habits-old.pdf'><button>DOWNLOAD</button></a></div>
+                                <div class='books__vote'><span>VOTE: <input type='checkbox' name='vote'></span></div>
+                            </div>
+                        </div>
+                        <div class='books__img item4'>
+                            <embed src='books/Atomic-Habits-old.pdf' height='90%' width='100%'>
+                            <div class='books__buttons'>
+                                <div class='books__download'><a href='books/Atomic-Habits-old.pdf'><button>DOWNLOAD</button></a></div>
+                                <div class='books__vote'><span>VOTE: <input type='checkbox' name='vote'></span></div>
+                            </div>
+                        </div>
+                      </div>";
         }
         ?>
         
